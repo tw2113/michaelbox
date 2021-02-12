2d77e9d03f4069a951a7e6d458614140abf9347b
Michael Beckwith <tw2113@gmail.com>
2021-02-12 00:50:45 -0600

    breadcrumbs

--- themes/blogsixteen-child/single.php
+++ themes/blogsixteen-child/single.php
@@ -12,6 +12,13 @@ get_header();?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
+        <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php
+			if ( function_exists( 'bcn_display' ) ) {
+				bcn_display();
+			}?>
+        </div>
+
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
